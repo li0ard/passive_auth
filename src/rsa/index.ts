dd36a5530cf806e6017e2673c9525e396f3fb8a7
Takdanai Vongtai <takdanaiv@rabbit.co.th>
2026-04-23 13:18:20 +0700

    feat: add rsassa pss support

--- src/rsa/index.ts
+++ src/rsa/index.ts
@@ -1,8 +1,10 @@
 import { bytesToNumberBE } from '@noble/curves/utils.js';
-import type { CertificateChoices } from '@peculiar/asn1-cms';
-import { id_rsaEncryption, RSAPublicKey, id_sha1WithRSAEncryption, id_sha256WithRSAEncryption, id_sha384WithRSAEncryption, id_sha512WithRSAEncryption, id_sha224WithRSAEncryption } from '@peculiar/asn1-rsa';
+import { DigestAlgorithmIdentifier, type CertificateChoices } from '@peculiar/asn1-cms';
+import { RsaSaPssParams, id_rsaEncryption, RSAPublicKey, id_sha1WithRSAEncryption, id_sha256WithRSAEncryption, id_sha384WithRSAEncryption, id_sha512WithRSAEncryption, id_sha224WithRSAEncryption, id_RSASSA_PSS } from '@peculiar/asn1-rsa';
 import { AsnConvert } from '@peculiar/asn1-schema';
 import * as rsa from 'micro-rsa-dsa-dh/rsa.js';
+import { HashHelper } from '../helpers/hash';
+import { RSAPaddingHelper } from '../helpers/rsa-padding';
 
 const PKCS1_ALGORITHMS_BY_OID: Readonly<Record<string, rsa.IPKCS>> = {
     [id_sha1WithRSAEncryption]: rsa.PKCS1_SHA1,
@@ -24,13 +26,36 @@ const convertSubjectPublicKey = (publicKey: ArrayBuffer): rsa.PublicKey => {
 
 /** Extract public key from X.509 certificate and verify signature with RSA PKCS#1 */
 export const rsaVerify = (cert: CertificateChoices, data: Uint8Array, signature: Uint8Array): boolean => {
-    if(!cert.certificate) throw new Error("Invalid certificate. Missing \"Certificate\" in \"CertificateChoices\"");
-    const algorithm = cert.certificate.tbsCertificate.subjectPublicKeyInfo.algorithm;
-    if(algorithm.algorithm !== id_rsaEncryption) throw new Error(`Invalid certificate. RSA certificate MUST use ${id_rsaEncryption} OID`);
+    if (!cert.certificate) throw new Error("Invalid certificate. Missing \"Certificate\" in \"CertificateChoices\"");
+    const publicKeyInfo = cert.certificate.tbsCertificate.subjectPublicKeyInfo
+    const algorithm = publicKeyInfo.algorithm;
+    if (algorithm.algorithm !== id_rsaEncryption) throw new Error(`Invalid certificate. RSA certificate MUST use ${id_rsaEncryption} OID`);
+    const signatureAlgorithm = cert.certificate.signatureAlgorithm.algorithm;
+    const isPkcs1 = PKCS1_ALGORITHMS_BY_OID[signatureAlgorithm]
+    const publicKey = convertSubjectPublicKey(publicKeyInfo.subjectPublicKey)
+    if (!isPkcs1) {
+        if (signatureAlgorithm === id_RSASSA_PSS) {
+            const rsaPSS = rsaPssVerify(cert);
+            return rsaPSS.verify(publicKey, data, signature)
+        }
+        throw new Error(`Unsupported RSA Signature Algorithm OID: ${signatureAlgorithm}`);
+    }
+    return isPkcs1.verify(publicKey, data, signature);
+}
 
-    const pkcs1 = PKCS1_ALGORITHMS_BY_OID[cert.certificate.signatureAlgorithm.algorithm];
-    if(!pkcs1) throw new Error(`Unsupported RSA PKCS#1 OID: ${cert.certificate.signatureAlgorithm.algorithm}`);
-    const publicKey = convertSubjectPublicKey(cert.certificate.tbsCertificate.subjectPublicKeyInfo.subjectPublicKey);
+function rsaPssVerify(cert: CertificateChoices) {
+    const rawPssParams = cert.certificate!.signatureAlgorithm.parameters as unknown as Uint8Array
+    if (!rawPssParams) throw new Error(`Missing RSA PSS Parameters ${cert.certificate!.signatureAlgorithm.algorithm}`)
+    const parsePssParams = AsnConvert.parse(rawPssParams, RsaSaPssParams);
+    // Get MGF1 hash algorithm
+    const mgf1Hash = AsnConvert.parse(parsePssParams.maskGenAlgorithm.parameters as unknown as Uint8Array, DigestAlgorithmIdentifier);
+    const pssParams = {
+        hashAlgorithm: HashHelper.resolve(parsePssParams.hashAlgorithm.algorithm),
+        maskGenAlgorithm: RSAPaddingHelper.resolve(parsePssParams.maskGenAlgorithm.algorithm),
+        maskGenHashAlgorithm: HashHelper.resolve(mgf1Hash.algorithm),
+        saltLength: parsePssParams.saltLength
+    };
 
-    return pkcs1.verify(publicKey, data, signature);
+    const rsaPSS = rsa.PSS(pssParams.hashAlgorithm, pssParams.maskGenAlgorithm(pssParams.maskGenHashAlgorithm), pssParams.saltLength);
+    return rsaPSS;
 }
